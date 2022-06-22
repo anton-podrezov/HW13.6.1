4ceedee0b0d61ff6e584f256b5bc50045cbd8765
jonsi <jonsi@DESKTOP-VI5FVR6>
2022-06-22 14:39:42 +0400

    Добавьте файлы проекта.

---
+++ HW13.6.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+
+string text = File.ReadAllText("C:\\Text1.txt");//Поис текста
+
+var textTxt = new List<string>();
+
+var linkedTextTxt = new LinkedList<string>();
+
+
+//Проверка вставки в List<T>
+
+var watchOne = Stopwatch.StartNew();
+
+textTxt.Add(text);
+
+double res1 = watchOne.Elapsed.TotalMilliseconds;
+
+Console.WriteLine($"Вставка в List<T>: {res1}  мс");
+
+
+//Проверка вставки в LinkedList<T>
+var watchTwo = Stopwatch.StartNew();
+
+linkedTextTxt.AddFirst(text);
+
+double res2 = watchTwo.Elapsed.TotalMilliseconds;
+
+Console.WriteLine($"Вставка в LinkedList<T>: {res2}  мс");
+
